a868ee69c9fe81a850b63018e4794689f698bc37
garland <garland.kan@carelon.com>
2023-04-27 16:52:05 -0700

    Updating google provider version constraint

--- modules/bootstrap/versions.tf
+++ modules/bootstrap/versions.tf
@@ -1,6 +1,9 @@
 terraform {
   required_version = ">= 0.15.3, < 2.0"
   required_providers {
-    google = { version = "~> 3.30" }
+    google = { 
+      # version = "~> 3.30" 
+      version = ">= 4.50, < 5.0"
+    }
   }
 }
