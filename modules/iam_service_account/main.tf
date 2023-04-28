9645a0eea542b810cafc4b5e480cb3a6e526afed
garland <garland.kan@carelon.com>
2023-04-27 17:44:54 -0700

    Adding project id

--- modules/iam_service_account/main.tf
+++ modules/iam_service_account/main.tf
@@ -11,6 +11,7 @@ resource "google_service_account" "this" {
 
 resource "google_project_iam_member" "this" {
   for_each = var.roles
+  project  = var.project_id
   role     = each.value
   member   = "serviceAccount:${google_service_account.this.email}"
 }
